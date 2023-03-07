b852f7925ebde05142674b58f36c2d93acb9b56f
ikhsanmardiansyah <ikhsanalfatih46@gmail.com>
2023-03-07 22:07:11 +0700

    Commit ke 2

--- src/main/java/com/juaracoding/CSmaster/repo/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.juaracoding.CSmaster.repo;
-
-import com.juaracoding.CSmaster.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student, Long>{
-
-}
